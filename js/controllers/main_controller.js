6554b2c9fd8c071d2f1d8fea2c867af5ef2115b2
June Shaw <june.shaw@me.com>
2016-02-24 00:09:11 -0700

    adding http

--- js/controllers/main_controller.js
+++ js/controllers/main_controller.js
@@ -2,6 +2,14 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http) {
   $http.get('https://api.github.com/zen').then(function(data){
     $scope.zenData = data.data;
   });
-  // $http.get('https://itunes.apple.com/search?term=jack+johnson').then(function($itunesData){
-  //   console.log('itunes data: ', itunesData.data);
+  // $http.get('https://itunes.apple.com/search?term=jack+johnson').then(function(data){
+  //   $scope.itunesData = data.data.results;
+  //   console.log('itunes data: ', $scope.itunesData);
+  // })
+  $http.get('itunes.json').success(function(data) {
+    $scope.itunesData = data.results;
+    console.log('itunes data : ', $scope.itunesData);
+  })
+
+
 }])
