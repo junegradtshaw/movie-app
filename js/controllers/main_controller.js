6c3e3fe2e0bd6e37898cd3a6ac7766233abfbc5e
June Shaw <june.shaw@me.com>
2016-02-24 23:48:40 -0700

    final

--- js/controllers/main_controller.js
+++ js/controllers/main_controller.js
@@ -1,26 +1,4 @@
 app.controller('MainController', ['$scope', function($scope) {
-// app.controller('MainController', ['$scope', '$http', function($scope, $http, $routeProvider) {
+
   $scope.title = "Angular OMDb API Search"
-  // $scope.searchTitle="";
-  // $scope.movies=[];
-
-  // $scope.searchMovie = function(movie) {
-  //   console.log(movie);
-  //   $http.get('http://www.omdbapi.com/?s='+movie.searchTitle).then(function(data){
-  //     // console.log('movie data: ', data);
-  //     // movieData = data;
-  //     $scope.movies = data.data.Search;
-  //     console.log($scope.movies);
-  //     return;
-  //   })
-  // }
-  // $http.get('https://api.github.com/zen').then(function(data){
-  //   $scope.zenData = data.data;
-  // });
-  // $http.get('itunes.json').success(function(data) {
-  //   $scope.itunesData = data.results;
-  //   console.log('itunes data : ', $scope.itunesData);
-  // })
-
-
 }])
--- js/controllers/show_controller.js
+++ js/controllers/show_controller.js
@@ -1,10 +1,9 @@
 app.controller('ShowController', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
 
-  $scope.movieID = $routeParams.movieID;
+  // $scope.movieID = $routeParams.movieID;
   $scope.movie;
 
-  $http.get('http://www.omdbapi.com/?i='+$scope.movieID).then(function(data){
+  $http.get('http://www.omdbapi.com/?i='+$routeParams.movieID).then(function(data){
     $scope.movie = data.data;
-    console.log('movie: ', $scope.movie);
   })
 }])
