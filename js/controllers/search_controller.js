5551f1d38a203fdbb2d5d182440b09fe78922da4
June Shaw <june.shaw@me.com>
2016-02-24 23:19:25 -0700

    final show page

--- js/controllers/search_controller.js
+++ js/controllers/search_controller.js
@@ -1,21 +1,9 @@
 app.controller('SearchController', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
   $scope.movieTitle = $routeParams.movieTitle;
-  console.log('routeParams: ', $routeParams);
-  console.log("you've reached the search controller with movie: ", $scope.movieTitle);
 
   $scope.movies;
-  //
-  // $scope.searchMovie = function() {
+
   $http.get('http://www.omdbapi.com/?s='+$scope.movieTitle).then(function(data){
     $scope.movies = data.data.Search;
-    // console.log($scope.movies);
-    // return;
   })
 }])
-
-
-// .controller('MyCtrl1', ['$scope','$routeParams', function($scope, $routeParams) {
-//   var param1 = $routeParams.param1;
-//   var param1 = $routeParams.param2;
-//   ...
-// }]);
--- js/controllers/show_controller.js
+++ js/controllers/show_controller.js
@@ -2,7 +2,6 @@ app.controller('ShowController', ['$scope', '$routeParams', '$http', function($s
 
   $scope.movieID = $routeParams.movieID;
   $scope.movie;
-  console.log("you've reached the show controller with movie: ", $scope.movieID);
 
   $http.get('http://www.omdbapi.com/?i='+$scope.movieID).then(function(data){
     $scope.movie = data.data;
